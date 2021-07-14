1def458d7b49768f68c715ff181f9a0e5892e37a
Austin Scott <austinscott1043@gmail.com>
2021-07-14 16:02:38 -0400

    Adding simple recursion scripts

---
+++ recursion.py
@@ -0,0 +1,15 @@
+
+def find_sum(n):
+    if n == 1:
+        return 1
+    return n + find_sum(n - 1)
+
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if __name__ == '__main__':
+    print(find_sum(7))
+    print(fibonacci(6))
